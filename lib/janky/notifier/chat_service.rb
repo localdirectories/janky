5dcdd6972e7c73cf8bfd1cb51acbdbb32ba726ac
Rob Jacoby <rjacoby@localdirectories.com.au>
2014-03-24 11:33:21 +1000

    add project and status to options

--- lib/janky/notifier/chat_service.rb
+++ lib/janky/notifier/chat_service.rb
@@ -3,7 +3,7 @@ module Janky
     class ChatService
       def self.completed(build)
         status = build.green? ? "was successful" : "failed"
-        color = build.green? ? "green" : "red"
+        color  = build.green? ? "green" : "red"
 
         message = "Build #%s (%s) of %s/%s %s (%ss) %s" % [
           build.number,
@@ -15,7 +15,7 @@ module Janky
           build.web_url
         ]
 
-        ::Janky::ChatService.speak(message, build.room_id, {:color => color})
+        ::Janky::ChatService.speak(message, build.room_id, { :color => color, project: build.repo_name, status: status })
       end
     end
   end
